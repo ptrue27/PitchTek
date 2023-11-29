2443c4d26a9767364ca24d50f5bc03c8ff74a501
ptrue27 <parkertrue27@gmail.com>
2023-11-28 16:05:31 -0800

    turned out, ball, and strike tracker into buttons

--- frontend/src/components/RecordGame.vue
+++ frontend/src/components/RecordGame.vue
@@ -138,13 +138,31 @@
                           <p class="out-ball-strike-text"> Outs</p>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">0</v-chip>
+                          <v-chip
+                              @click="setOuts(0)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              0
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">1</v-chip>
+                          <v-chip
+                              @click="setOuts(1)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              1
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">2</v-chip>
+                          <v-chip
+                              @click="setOuts(2)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              2
+                          </v-chip>
                         </v-col>
                       </v-row>
                       
@@ -154,16 +172,40 @@
                           <p class="out-ball-strike-text">Balls</p>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">0</v-chip>
+                          <v-chip
+                              @click="setBalls(0)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              0
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">1</v-chip>
+                          <v-chip
+                              @click="setBalls(1)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              1
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">2</v-chip>
+                          <v-chip
+                              @click="setBalls(2)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              2
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">3</v-chip>
+                          <v-chip
+                              @click="setBalls(3)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              3
+                          </v-chip>
                         </v-col>
                       </v-row>
 
@@ -173,13 +215,31 @@
                           <p class="out-ball-strike-text">Strikes</p>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">0</v-chip>
+                          <v-chip
+                              @click="setStrikes(0)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              0
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">1</v-chip>
+                          <v-chip
+                              @click="setStrikes(1)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              1
+                          </v-chip>
                         </v-col>
                         <v-col cols="2">
-                          <v-chip size="x-small" class="mr-1">2</v-chip>
+                          <v-chip
+                              @click="setStrikes(2)" 
+                              size="x-small" 
+                              class="mr-1"
+                          >
+                              2
+                          </v-chip>
                         </v-col>
                       </v-row>
 
@@ -304,14 +364,14 @@
   export default {
     data() {
       return {
-        homeScore: '',
-        awayScore: '',
         homeTeams: ['Team1', 'Team2', 'Team3'],
         awayTeams: ['Team1', 'Team2', 'Team3'],
-        innings: ['1∧', '1∨','2∧', '2∨', '3∧', '3∨', '4∧', '4∨','5∧', '5∨', '6∧', '6∨', '7∧', '7∨','8∧', '8∨', '9∧', '9∨', '10∧', '10∨','11∧', '11∨', '12∧', '12∨', '13∧', '13∨','14∧', '14∨', '15∧', '15∨', '16∧', '16∨','17∧', '17∨', '18∧', '18∨', '19∧', '19∨','20∧', '20∨', '21∧', '21∨', '22∧', '22∨','23∧', '23∨', '24∧', '24∨', '25∧', '25∨','26∧', '26∨', '27∧', '27∨', '28∧', '28∨','29∧', '29∨', '30∧', '30∨', '31∧', '31∨','32∧', '32∨', '33∧', '33∨', '34∧', '34∨','35∧', '35∨', '36∧', '36∨'],
-        outs: ['0', '1', '2', '3'],
-        balls: ['0', '1', '2', '3', '4'],
-        strikes: ['0', '1', '2', '3']
+        homeScore: '0',
+        awayScore: '0',
+        innings: ['1∧', '1∨','2∧', '2∨', '3∧', '3∨', '4∧', '4∨','5∧', '5∨', '6∧', '6∨', '7∧', '7∨','8∧', '8∨', '9∧', '9∨', '10∧', '10∨','11∧', '11∨', '12∧', '12∨', '13∧', '13∨','14∧', '14∨', '15∧', '15∨', '16∧', '16∨','17∧', '17∨', '18∧', '18∨', '19∧', '19∨','20∧', '20∨', '21∧', '21∨', '22∧', '22∨','23∧', '23∨', '24∧', '24∨', '25∧', '25∨','26∧', '26∨', '27∧', '27∨', '28∧', '28∨','29∧', '29∨', '30∧', '30∨', '31∧', '31∨','32∧', '32∨', '33∧', '33∨', '34∧', '34∨','35∧', '35∨', '36∧', '36∨', '37∧', '37∨','38∧', '38∨', '39∧', '39∨', '40∧', '40∨','41∧', '41∨', '42∧', '42∨', '43∧', '43∨','44∧', '44∨', '45∧', '45∨', '46∧', '46∨', '47∧', '47∨','48∧', '48∨', '49∧', '49∨', '50∧', '50∨'],
+        outNumber: 0,
+        ballNumber: 0,
+        strikeNumber: 0,
       };
     },
     methods: {
@@ -325,6 +385,15 @@
       },
       toggleBase(baseNumber) {
         console.log('Toggled base:', baseNumber);
+      },
+      setOuts(outNumber) {
+        console.log('Changed number of outs:', outNumber)
+      },
+      setBalls(ballNumber) {
+        console.log('Changed number of balls:', ballNumber)
+      },
+      setStrikes(strikeNumber) {
+        console.log('Changed number of strikes:', strikeNumber)
       },
     },
   };
