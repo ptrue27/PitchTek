bfc83b74f79d00afec989ff52743499041834836
ptrue27 <ptrue@nevada.unr.edu>
2024-02-15 00:16:06 -0800

    added scraper for batter stats

---
+++ backend/databases/README.md
@@ -0,0 +1,7 @@
+## teams.db 
+TEAMS (id, name)
+ROSTERS (id, name, position, team_id)
+
+## players.db
+BATTERS (id, name, img, games, pa, avg, obp, slg, ops, hits, hr)
+PITCHERS (id, name, img, era, kper9, bbper9)
--- backend/scrape.py
+++ backend/scrape.py
@@ -3,13 +3,22 @@ import sql_utils
 import mlbstatsapi
 import time
 from random import uniform
-import os
 
 mlb = mlbstatsapi.Mlb()
 
 
 def rest():
     time.sleep(5 + uniform(0, 2))
+
+
+def view_database(table_name, rows=None):
+    conn = sqlite3.connect(sql_utils.db_files[table_name])
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM {table_name}')
+    rows = cursor.fetchmany(rows)
+    for row in rows:
+        print(row)
+    conn.close()
 
 
 def get_teams():
@@ -76,6 +85,39 @@ def get_rosters():
     conn.close()
 
 
+def get_batters():
+    player_ids = sql_utils.get_table('ROSTERS')['id']
+    table_name = 'BATTERS'
+    db_file = sql_utils.db_files[table_name]
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    i = 0
+    for id in player_ids:
+        stats = mlb.get_player_stats(id, stats=['season'], groups=['hitting'])
+        if len(stats) == 0:
+            i += 1
+            continue
+        stats = stats['hitting']['season'].splits[0]
+        name = stats.player.fullname
+        stat = stats.stat
+        img = 'https://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30836.png'
+        
+        if stat.numberofpitches > 0:
+            print(id, name, img, stat.gamesplayed, stat.plateappearances, stat.avg, stat.obp, stat.slg, stat.ops, stat.hits, stat.homeruns)
+            cursor.execute('''INSERT INTO BATTERS (id, name, img, games, pa, avg, obp, slg, ops, hits, hr) 
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+                    (id, name, img, stat.gamesplayed, stat.plateappearances, stat.avg, stat.obp, stat.slg, stat.ops, stat.hits, stat.homeruns))
+
+        i += 1
+        if i > 10:
+            rest()
+            i = 0
+
+    conn.commit()
+    conn.close()
+
 if __name__ == '__main__':
-    get_teams()
-    get_rosters()
+    #get_teams()
+    #get_rosters()
+    get_batters()
