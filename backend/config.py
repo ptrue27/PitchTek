f8eaa89a371e3ab1676b8b63d812c6b1116141d0
ptrue27 <ptrue@nevada.unr.edu>
2024-03-06 08:44:11 -0800

    simplified Config

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import os
 
-DB_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "databases")
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-@JHh#*&##98h8sdmbq9h9E7FG5&hfsdlf)'
+    DB_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "databases")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DB_DIR, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-@JHh#*&##98h8sdmbq9h9E7FG5&hfsdlf)'
