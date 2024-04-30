f038694307f52514815f9c276fbd86d5ec3dc2c8
ptrue27 <ptrue@nevada.unr.edu>
2024-04-29 17:40:44 -0700

    fixed URL

--- frontend/src/views/MatchupView.vue
+++ frontend/src/views/MatchupView.vue
@@ -242,7 +242,7 @@
       let formData = new FormData();
       formData.append('file', file);
 
-      host = "http://" + this.$store.state.host + "/api/upload_csv";
+      const host = "http://" + this.$store.state.host + "/api/upload_csv";
       axios.post(host, formData)
         .then(response => {
           this.players = response.data;
